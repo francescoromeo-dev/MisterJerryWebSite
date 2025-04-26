3a24d18df3f0b284f00184d15cdd4902987a9038
francescoromeo-dev <romeofrancesco301@gmail.com>
2025-04-26 11:00:22 +0200

    Added js function for size cover

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Button menu small size
 let menu = document.querySelector('#menu-icon');
 let navigationCenter = document.querySelector('.navigation-center');
 
@@ -5,3 +6,11 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navigationCenter.classList.toggle('open')
 }
+
+//Dynamic height size fix
+function setHeroHeight() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  window.addEventListener('resize', setHeroHeight);
+  window.addEventListener('load', setHeroHeight);
